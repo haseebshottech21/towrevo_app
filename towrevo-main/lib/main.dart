919943eb15306816a400ba4caefa790eeb6f2c5f
lovekumarshottech <love.kumar@shottech.co>
2022-01-25 18:58:51 +0500

    toast hide

--- towrevo-main/lib/main.dart
+++ towrevo-main/lib/main.dart
@@ -65,9 +65,9 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
-  Fluttertoast.showToast(
-      msg: 'background message ${message.notification!.body}');
-  print("Handling a background message ${message}");
+  // Fluttertoast.showToast(
+  //     msg: 'background message ${message.notification!.body}');
+  // print("Handling a background message ${message}");
 }
 
 class MyApp extends StatefulWidget {
