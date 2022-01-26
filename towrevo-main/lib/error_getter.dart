8fac9df8b8cc7bcfa9285e34508bdececdbf0f60
lovekumarshottech <love.kumar@shottech.co>
2022-01-26 19:32:23 +0500

    f

--- towrevo-main/lib/error_getter.dart
+++ towrevo-main/lib/error_getter.dart
@@ -34,10 +34,9 @@ class ErrorGetter {
   }
 
   dynamic companyDescriptionErrorGetter(String value) {
-    
     if (value.isEmpty) {
       return 'Please Enter Company Description';
-    } else if ( RegExp(r'\d').hasMatch(value.trim())) {
+    } else if (RegExp(r'\d').hasMatch(value.trim())) {
       return 'Numbers are not allow.';
     } else if (value.length > 300) {
       return 'Company Description length Should be less than 300';
